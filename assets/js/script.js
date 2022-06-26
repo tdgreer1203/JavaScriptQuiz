40f46aa74d546d689f344c6a023f40d71d870d67
Theo <tdgreer1203@gmail.com>
2022-06-26 11:33:04 -0400

    Working on JS Classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,40 @@
-var highScoresBtn = document.getElementById('viewHighScores');
+//Question Object
+const question = [ {
+    questionText: "",
+    answers: [
+        { text: '', correct: false },
+        { text: '', correct: false },
+        { text: '', correct: false },
+        { text: '', correct: false }
+    ]
+}
+]
+
+//Section Variables
 var quizStartSection = document.querySelector('.quizStart');
 var questionViewSection = document.querySelector('.questionView');
 var highScoresFormSection = document.querySelector('.highScoresForm');
 var highScoresSection = document.querySelector('.highScores');
+
+//Button Variables
+var highScoresBtn = document.getElementById('viewHighScores');
+var startBtn = document.getElementById('startQuiz'); 
+var goBackBtn = document.getElementById('goBackBtn');
+
 var timerDisplay = document.getElementById('timer');
 var sectionArray = [quizStartSection, questionViewSection, highScoresFormSection, highScoresSection];
 var timeLeft = 60;
 
 highScoresBtn.addEventListener('click', function() {
     showSection(highScoresSection);
+});
+
+startBtn.addEventListener('click', function() {
+    showSection(questionViewSection);
+});
+
+goBackBtn.addEventListener('click', function() {
+    showSection(highScoresFormSection);
 });
 
 function startTimer() {
