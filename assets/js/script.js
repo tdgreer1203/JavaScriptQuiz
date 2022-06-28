c78e14b4e0148929869cbca243041d4578e12e3d
Theo <tdgreer1203@gmail.com>
2022-06-28 06:37:30 -0400

    Working on Question Format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
-//Question Object with Answers
-const question = [ {
-    questionText: "",
-    answers: [
-        { text: '', correct: false },
-        { text: '', correct: false },
-        { text: '', correct: false },
-        { text: '', correct: false }
-    ]
+//Declaring Objects (Question and HighScoreEntry)
+const question = {
+    text: 'What is the capital of Texas',
+    choices: [{
+        text: 'Austin',
+        status: true
+    }, {
+        text: 'Dallas',
+        status: false
+    }, {
+        text: 'Houston',
+        status: false
+    }, {
+        text: 'San Antonio',
+        status: false
+    }]
 }
-]
+
+const highScoreEntry = {
+    initials: "",
+    score: 0
+}
 
 //Section Variables
 var quizStartSection = document.querySelector('.quizStart');
@@ -16,21 +27,33 @@ var questionViewSection = document.querySelector('.questionView');
 var highScoresFormSection = document.querySelector('.highScoresForm');
 var highScoresSection = document.querySelector('.highScores');
 
+//Element Variables
+var questionText = document.getElementById('questionText');
+var choice1 = document.getElementById('choice1');
+var choice2 = document.getElementById('choice2');
+var choice3 = document.getElementById('choice3');
+var choice4 = document.getElementById('choice4');
+
 //Button Variables
 var highScoresBtn = document.getElementById('viewHighScores');
 var startBtn = document.getElementById('startQuiz'); 
 var goBackBtn = document.getElementById('goBackBtn');
 
+//Data Variables
 var timerDisplay = document.getElementById('timer');
 var sectionArray = [quizStartSection, questionViewSection, highScoresFormSection, highScoresSection];
 var timeLeft = 60;
+var score = 0;
+var highScores = [];
 
 highScoresBtn.addEventListener('click', function() {
     showSection(highScoresSection);
 });
 
 startBtn.addEventListener('click', function() {
+    showQuestion(questions[0]);
     showSection(questionViewSection);
+    startTimer();
 });
 
 goBackBtn.addEventListener('click', function() {
@@ -59,3 +82,13 @@ function showSection(screen) {
         }
     }
 }
+
+function showQuestion(question) {
+    questionText.text = question.text;
+    choice1.text = question.choices[0].text;
+    choice2.text = question.cohices[0].text;
+    choice3.text = question.choices[0].text;
+    choice4.text = question.choices[0].text;
+}
+
+console.log(testQuestion.text);
