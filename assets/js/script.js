9253beae548f5b6f4b436ff7ab4efc86a8b6b465
Theo <tdgreer1203@gmail.com>
2022-06-30 05:24:53 -0400

    Completed Quiz Now Need Persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,21 +81,24 @@ var choice3 = document.getElementById('choice3');
 var choice4 = document.getElementById('choice4');
 var answerDisplay = document.getElementById('answerDisplay');
 var yourScore = document.getElementById('yourScore');
+var hsForm = document.getElementById('hsForm');
 var choiceButtons = [choice1, choice2, choice3, choice4];
 
 //Button Variables
 var highScoresBtn = document.getElementById('viewHighScores');
 var startBtn = document.getElementById('startQuiz'); 
 var goBackBtn = document.getElementById('goBackBtn');
+var clearBtn = document.getElementById('clearBtn');
 
 //Form Variables
 var initialsField = document.getElementById('initials');
 var hsForm = document.getElementById('hsForm');
+var scoreHolder = document.getElementById('scoreHolder');
 
 //Data Variables
 var timerDisplay = document.getElementById('timer');
 var sectionArray = [quizStartSection, questionViewSection, highScoresFormSection, highScoresSection];
-var timeLeft = 60;
+var timeLeft = 10;
 var score = 0;
 var highScores = [];
 var questions = [question1, question2, question3, question4];
@@ -114,16 +117,33 @@ startBtn.addEventListener('click', function() {
 });
 
 goBackBtn.addEventListener('click', function() {
-    showSection(highScoresFormSection);
+    resetValues();
+    showSection(quizStartSection);
+});
+
+clearBtn.addEventListener('click', function() {
+    highScores = [];
+    populateScores();
 });
 
 function startTimer() {
+    if(timeLeft < 1) {
+        timeLeft = 0;
+        endQuiz();
+    } else {
+        timeLeft--;
+    }
     timerDisplay.innerHTML = timeLeft;
-    timeLeft--;
 }
 
 function wrongAnswer() {
-    timeLeft -= 5;
+    if(timeLeft < 6) {
+        timeLeft = 0;
+        timerDisplay.innerHTML = timeLeft;
+        endQuiz();
+    } else {
+        timeLeft -= 5;
+    }
 }
 
 function showSection(screen) {
@@ -139,6 +159,15 @@ function showSection(screen) {
 }
 
 function showQuestion(question) {
+    var tempValue = null;
+
+    for(var i = 0; i < question.choices.length; i++) {
+        var randomNumber = Math.floor(Math.random() * (4));
+        tempValue = question.choices[i];
+        question.choices[i] = question.choices[randomNumber];
+        question.choices[randomNumber] = tempValue;
+    }
+
     questionText.innerHTML = question.text;
     choice1.innerHTML = question.choices[0].text;
     choice2.innerHTML = question.choices[1].text;
@@ -147,6 +176,10 @@ function showQuestion(question) {
 }
 
 function nextQuestion() {
+    choice1.classList.remove('selectedBtn');
+    choice2.classList.remove('selectedBtn');
+    choice3.classList.remove('selectedBtn');
+    choice4.classList.remove('selectedBtn');
     if(currentQuestion < 3) {
         currentQuestion++;
         showQuestion(questions[currentQuestion]);
@@ -163,6 +196,7 @@ function endQuiz() {
 }
 
 function answerSelected(answer) {
+    disableButtons();
     answerDisplay.classList.remove('hidden');
     if(answer) {
         answerDisplay.innerHTML = "<h2 style='color: green;'>Correct</h2>";
@@ -175,40 +209,77 @@ function answerSelected(answer) {
     }
     setTimeout(function() {
         answerDisplay.classList.add('hidden');
+        enableButtons();
         nextQuestion();
-    }, 2000);
+    }, 1500);
 }
 
-function addHighScore() {
+function disableButtons() {
+    choice1.classList.add('disableBtn');
+    choice2.classList.add('disableBtn');
+    choice3.classList.add('disableBtn');
+    choice4.classList.add('disableBtn');
+}
+
+function enableButtons() {
+    choice1.classList.remove('disableBtn');
+    choice2.classList.remove('disableBtn');
+    choice3.classList.remove('disableBtn');
+    choice4.classList.remove('disableBtn');
+}
+
+function addHighScore(event) { 
+    event.preventDefault();
     if(!initialsField.value) {
         alert('Please enter your initials');
     } else {
         var newScore = {
-            initials:  initialText,
+            initials:  initialsField.value,
             score: score
         }
-    
         highScores.push(newScore);
-        showSection(highScoresSection);
+        populateScores();
     }
 }
 
 function populateScores() {
+    scoreHolder.innerHTML = "";
+    if(highScores.length > 0) {
+        for(var i = 0; i < highScores.length; i++) {
+            scoreHolder.innerHTML += "<li>" + highScores[i].initials + " - " + highScores[i].score + "</li>";
+        }
+    } else {
+        scoreHolder.innerHTML = "<li>There are not scores to display at this time</li>";
+    }
+    showSection(highScoresSection);
+}
 
+function resetValues() {
+    score = 0;
+    timeLeft = 60;
+    timerDisplay.innerHTML = timeLeft;
+    currentQuestion = 0;
+    initialsField.value = "";
 }
 
 choice1.addEventListener("click", function() {
+    choice1.classList.add('selectedBtn');
     answerSelected(questions[currentQuestion].choices[0].status);
 });
 
 choice2.addEventListener("click", function() {
+    choice2.classList.add('selectedBtn');
     answerSelected(questions[currentQuestion].choices[1].status);
 });
 
 choice3.addEventListener("click", function() {
+    choice3.classList.add('selectedBtn');
     answerSelected(questions[currentQuestion].choices[2].status);
 });
 
 choice4.addEventListener("click", function() {
+    choice4.classList.add('selectedBtn');
     answerSelected(questions[currentQuestion].choices[3].status);
 });
+
+hsForm.addEventListener("submit", addHighScore);
